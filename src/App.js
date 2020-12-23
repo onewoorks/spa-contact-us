51350bd0a0f191341b81562eec4d460c41596f94
onewoorks <irwanbibrahim@onewoorks.com>
2020-12-24 07:47:27 +0800

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Lottie from 'react-lottie'
+import animationData from './lotties/contact-us'
 
 function App() {
+
+  const defaultOption = {
+    loop: true,
+    autoPlay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preservedAspectRatio: "xMidYMid slice"
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+      <Lottie options={defaultOption} height={500} width={750} />
+      <div style={{fontSize:'1.2em', textTransform:"uppercase", lineHeight:'1em'}}>
+      <div>Modul ini belum diaktifkan</div>
+      <div>Hubungi Kami</div>
+      </div>
+      <div style={{marginTop:10, textDecoration:'italic', fontSize:'0.6em'}}><i>Onewoorks Solutions | Web Application Developer</i></div>
+      </div>
     </div>
   );
 }
